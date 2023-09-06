9f7606631f6b0a1685af06d3121729d801de4abf
allanteone <allanteone@gmail.com>
2023-09-06 21:57:13 +0300

    hw-29

--- src/telran/interviews/InterviewTasks.java
+++ src/telran/interviews/InterviewTasks.java
@@ -1,6 +1,7 @@
 package telran.interviews;
 
-import java.util.Arrays;
+import java.time.LocalDate;
+import java.util.*;
 
 public class InterviewTasks {
 	
@@ -13,8 +14,15 @@ public class InterviewTasks {
 		Arrays.stream(ar).parallel().forEach(System.out::println);
 	}
 	
-	public static List<DateRole> rolesInDates(List<DateRole> datesRoles, List<LocalDate> dates) {
+	static public List<DateRole> rolesInDates(List<DateRole> datesRoles, List<LocalDate> dates) {
 		
+		TreeMap<LocalDate, String> mapDateRole = new TreeMap<>();
+		datesRoles.forEach(dr -> mapDateRole.put(dr.date(), dr.role()));
+		return dates.stream().map(d -> {
+			Map.Entry<LocalDate, String> entry = mapDateRole.floorEntry(d);
+			DateRole dr = new DateRole(d, entry == null ? null : entry.getValue());
+			return dr;
+		}).toList();
 	}
 
 }
